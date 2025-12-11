d531da1b4bfe097af6b21a460f568e40e6c18fb5
hasnain <hasnainking110786@gmail.com>
2025-12-11 15:20:11 +0500

    changing in file

--- week 3/game/script.js
+++ week 3/game/script.js
@@ -1,6 +1,6 @@
 // --- JAVASCRIPT LOGIC ---
 
-// 1. create an array of possible answer (classic +fun/sarcastic)
+// 1. create an array of possible answers (classic + fun/sarcastic)
 const answers = [
     "It is certain.",
     "Yes definitely.",
@@ -10,28 +10,28 @@ const answers = [
     "Chances are not so good.",
     "Very doubtful.",
     "404 Error : Fate not found.",
-    "Only if you do alittle dance first.",
-    "You are aabout die!", 
+    "Only if you do a little dance first.",
+    "You are about die!", 
     "The stars say... meh.",
-    "Askyour dog,they know better.",
-    "Comuter says no.",
-    "I'm on a coffrr break, ask late.",
-    "Yes but you won't like the  consequences.",
+    "Ask your dog,they know better.",
+    "Computer says no.",
+    "I'm on a coffee break, ask later.",
+    "Yes but you won't like the consequences.",
     "You need to consult a doctor.",
-    "Ypu wifi connection to dstiny is is weak.",
+    "Your wifi connection to destiny is weak.",
     "Fate is currently stuck.",
     "Chances are great...for someone else"
 ]
 
 //2. select the important HTML elements
-const getAnswerbutton = document.querySelector("#get-answer-btn");
+const getAnswerButton = document.querySelector("#get-answer-btn");
 const answerDisplay = document.querySelector("#answer");
 
 //3. the function runs on click
 function showRandomAnswer () {
     const randomIndex = Math.floor(Math.random() * answers.length);
-    const RandomAnswer = answers[randomIndex];
-    answerDisplay.textContent = RandomAnswer; 
+    const randomAnswer = answers[randomIndex];
+    answerDisplay.textContent = randomAnswer; 
 } 
 //4. We use the variable 'getAnswerbutton we defined above.
-getAnswerbutton.addEventListener("click", showRandomAnswer); 
+getAnswerButton.addEventListener("click", showRandomAnswer); 
